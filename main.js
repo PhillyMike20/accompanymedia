08314aa11d94d23d217b6af96d1e7902c493d889
Mike <mstirmii@gmail.com>
2021-03-29 12:31:45 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var text = 'ACCOMPANY'; 
+
+var textElements = text.split("").map(function(c) {
+  return $('<span id="' + c + '">' + c + '</span>');
+});
+
+var el = $('#letters');
+var delay = 152; 
+textElements.forEach(function(e, i) {
+  el.append(e);
+  e.hide();
+  setTimeout(function() {
+    e.fadeIn(300)
+  }, 100 + i * delay)
+  setInterval(function(){document.getElementById('logo').classList.add("m-fadeIn")}, 1600);
+  setInterval(function(){document.getElementById('nav-list').classList.add("m-fadeIn")}, 1800);
+ 
+})
